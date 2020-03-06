db3a512376288c422b45dee634e09304d9a23521
meenurajapandian <meenu.shreeji@googlemail.com>
2020-03-05 21:51:31 -0500

    Another scroll try

--- docs/script3.js
+++ docs/script3.js
@@ -4,38 +4,39 @@ var slide = 0;
 var maxslide = 10;
 
 
-window.addEventListener('wheel', ( evt ) => {
-  console.timeStamp()
-  if (checkScrollDirectionIsUp(event)) {
-    console.log('Up');
-    console.log(slide);
-    slide = slide - 1;
-    if (slide < 0){
-      slide = 0;
-    }
-    console.log(slide)
-    window.scrollTo({
-      top: slide * windowHeight,
-      left: 0,
-      behavior: 'smooth'
-    });
+window.addEventListener('wheel', _.throttle(checkScrollDirection, 1000, {trailing: false, leading: true }));
 
-  } else {
-    console.log('Down');
-    console.log(slide);
-    slide = slide + 1;
-    if (slide > maxslide){
-      slide = maxslide;
-    }
-    console.log(slide)
-    window.scrollTo({
-      top: slide * windowHeight,
-      left: 0,
-      behavior: 'smooth'
-    });
-  }
-}, _.throttle(callback, 1000, {trailing: false, leading: true }));
 
+function checkScrollDirection(event) {
+    if (checkScrollDirectionIsUp(event)) {
+      console.log('Up');
+      console.log(topPosition);
+      slide = slide - 1;
+      if (slide < 0){
+        slide = 0;
+      }
+      console.log(slide)
+      window.scrollTo({
+        top: slide * windowHeight,
+        left: 0,
+        behavior: 'smooth'
+      });
+
+    } else {
+      console.log('Down');
+      console.log(topPosition);
+      slide = slide + 1;
+      if (slide > maxslide){
+        slide = maxslide;
+      }
+      console.log(slide)
+      window.scrollTo({
+        top: slide * windowHeight,
+        left: 0,
+        behavior: 'smooth'
+      });
+    }
+}
 
 
 function checkScrollDirectionIsUp(event) {
